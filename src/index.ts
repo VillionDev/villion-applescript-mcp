fd51fee7c436484bba947b214ec54c70d531dc17
Damvilion <noelohaeri@gmail.com>
2025-07-19 23:34:51 -0700

    updated git ignore + others

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { openApplication, openApplicationSchema } from "./tools";
 
-// Create server instance
 const server = new McpServer({
   name: "villion-mcp",
   version: "1.0.0",
